b99187215c07a3ff5f25de7894c09ee493df906b
Glatzel <893016099@qq.com>
2025-06-17 17:22:31 +0800

    f

--- crates/nmea/src/context.rs
+++ crates/nmea/src/context.rs
@@ -1,12 +1,17 @@
 use std::fs::File;
+use std::io::Write;
 use std::path::Path;
+use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 
 use lru::LruCache;
 use miette::IntoDiagnostic;
 
+use crate::nmea_data::{Dhv, GenericNmeaData, Gga, Gll, Gsa, NmeaDataType, Vtg, Zda};
+use crate::{INmeaData, NavigationSystem};
+
 pub struct NmeaContext {
-    cache: Arc<Mutex<LruCache<&'static str, crate::nmea_data::GenericNmeaData>>>,
+    cache: Arc<Mutex<LruCache<NmeaDataType, crate::nmea_data::GenericNmeaData>>>,
     output: Option<Mutex<File>>,
 }
 impl NmeaContext {
@@ -23,22 +28,59 @@ impl NmeaContext {
         })
     }
     pub fn push(&self, sentense: &str) -> miette::Result<&Self> {
-        let data = crate::nmea_data::GenericNmeaData::try_from(sentense)?;
-        let key: &'static str = match data {
-            crate::nmea_data::GenericNmeaData::DHV(_) => "dhv",
-            crate::nmea_data::GenericNmeaData::GGA(_) => "gga",
-            crate::nmea_data::GenericNmeaData::GLL(_) => "gll",
-            crate::nmea_data::GenericNmeaData::GSA(_) => "gsa",
-            crate::nmea_data::GenericNmeaData::VTG(_) => "vtg",
-            crate::nmea_data::GenericNmeaData::ZDA(_) => "zda",
-            crate::nmea_data::GenericNmeaData::Other(_) => "other",
+        let key = NmeaDataType::from_str(sentense)?;
+        let navigation_system = NavigationSystem::from_str(&sentense[1..3]).into_diagnostic()?;
+
+        let data = match key {
+            NmeaDataType::DHV => {
+                GenericNmeaData::DHV(Dhv::parse_sentense(sentense, navigation_system)?)
+            }
+            NmeaDataType::GGA => {
+                GenericNmeaData::GGA(Gga::parse_sentense(sentense, navigation_system)?)
+            }
+            NmeaDataType::GLL => {
+                GenericNmeaData::GLL(Gll::parse_sentense(sentense, navigation_system)?)
+            }
+            NmeaDataType::GSA => {
+                GenericNmeaData::GSA(Gsa::parse_sentense(sentense, navigation_system)?)
+            }
+            NmeaDataType::VTG => {
+                GenericNmeaData::VTG(Vtg::parse_sentense(sentense, navigation_system)?)
+            }
+            NmeaDataType::ZDA => {
+                GenericNmeaData::ZDA(Zda::parse_sentense(sentense, navigation_system)?)
+            }
+            NmeaDataType::Other => GenericNmeaData::Other(sentense.to_string()),
         };
 
-        {
-            let mut cache = self.cache.lock().unwrap();
-            cache.put(key, data);
-        }
+        let mut cache = self.cache.lock().unwrap();
+        cache.put(key, data);
 
+        if let Some(file_mutex) = &self.output {
+            if let Ok(mut file) = file_mutex.lock() {
+                let _ = writeln!(file, "{}", sentense);
+            }
+        }
         Ok(self)
     }
+    /// Get a cached NMEA data value by type.
+    pub fn get(&self, key: NmeaDataType) -> Option<GenericNmeaData> {
+        let mut cache = self.cache.lock().unwrap();
+        cache.get(&key).cloned()
+    }
+
+    /// Check if a sentence type exists in the cache.
+    pub fn contains(&self, nmea_type: NmeaDataType) -> bool {
+        let cache = self.cache.lock().unwrap();
+        cache.contains(&nmea_type)
+    }
+
+    /// Get number of items currently cached.
+    pub fn len(&self) -> usize {
+        let cache = self.cache.lock().unwrap();
+        cache.len()
+    }
+
+    /// Check if the cache is empty.
+    pub fn is_empty(&self) -> bool { self.len() == 0 }
 }
--- crates/nmea/src/nmea_data.rs
+++ crates/nmea/src/nmea_data.rs
@@ -11,43 +11,48 @@ pub use dhv::*;
 pub use gga::*;
 pub use gll::*;
 pub use gsa::*;
-use miette::IntoDiagnostic;
 pub use vtg::*;
 pub use zda::*;
 
-use crate::{INmeaData, NavigationSystem};
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Hash, Eq, PartialEq)]
+pub enum NmeaDataType {
+    DHV,
+    GGA,
+    GLL,
+    GSA,
+    VTG,
+    ZDA,
+    Other,
+}
+impl FromStr for NmeaDataType {
+    type Err = miette::Report;
 
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+    fn from_str(sentense: &str) -> Result<Self, Self::Err> {
+        if sentense.len() < 6 {
+            miette::bail!("Invalid sentense: {}", sentense);
+        }
+        let out = match &sentense[3..6] {
+            "DHV" => Self::DHV,
+            "GGA" => Self::GGA,
+            "GLL" => Self::GLL,
+            "GSA" => Self::GSA,
+
+            "VTG" => Self::VTG,
+            "ZDA" => Self::ZDA,
+
+            _ => Self::Other,
+        };
+        Ok(out)
+    }
+}
+#[derive(Clone)]
 pub enum GenericNmeaData {
     DHV(crate::nmea_data::Dhv),
     GGA(crate::nmea_data::Gga),
     GLL(crate::nmea_data::Gll),
     GSA(crate::nmea_data::Gsa),
-    // GSV(crate::nmea_data::Gsv),
     VTG(crate::nmea_data::Vtg),
     ZDA(crate::nmea_data::Zda),
 
     Other(String),
-}
-impl TryFrom<&str> for GenericNmeaData {
-    type Error = miette::Report;
-
-    fn try_from(sentense: &str) -> miette::Result<Self> {
-        if sentense.len() < 6 {
-            miette::bail!("Invalid sentense: {}", sentense);
-        }
-        let navigation_system = NavigationSystem::from_str(&sentense[1..3]).into_diagnostic()?;
-        let out = match &sentense[3..6] {
-            "DHV" => Self::DHV(Dhv::parse_sentense(sentense, navigation_system)?),
-            "GGA" => Self::GGA(Gga::parse_sentense(sentense, navigation_system)?),
-            "GLL" => Self::GLL(Gll::parse_sentense(sentense, navigation_system)?),
-            "GSA" => Self::GSA(Gsa::parse_sentense(sentense, navigation_system)?),
-            // "GSV" => Ok(Self::GSV),
-            "VTG" => Self::VTG(Vtg::parse_sentense(sentense, navigation_system)?),
-            "ZDA" => Self::ZDA(Zda::parse_sentense(sentense, navigation_system)?),
-
-            other => GenericNmeaData::Other(other.to_string()),
-        };
-        Ok(out)
-    }
 }
