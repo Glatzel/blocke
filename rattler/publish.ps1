3a4a9bafd35beb3e10813b9d1103ff35ef32a969
Glatzel <893016099@qq.com>
2025-06-22 11:39:26 +0800

    g

--- rattler/publish.ps1
+++ rattler/publish.ps1
@@ -4,15 +4,8 @@ Set-Location ..
 
 foreach ($pkg_file in Get-ChildItem "./$pkg/output/*/*.conda" -Recurse -ErrorAction Continue) {
     Write-Output "::group:: upload $pkg"
-    if(Test-Path "./packages/$pkg/PUBLIC") {
         Write-Output "$pkg is a public package"
         pixi run rattler-build upload prefix -s -c glatzel $pkg_file
-    }
-    else {
-        Write-Output "$pkg is a private package"
-        pixi run rattler-build upload prefix -s -c glatzel-private $pkg_file
-    }
-
     $LASTEXITCODE = 0
     Write-Output "::endgroup::"
 }
