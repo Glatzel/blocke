50ce934e98f856fbb039c9f4026b473b08fcdfa5
Glatzel <893016099@qq.com>
2025-06-25 11:36:55 +0800

    f

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,10 +1,17 @@
 param (
-    [Parameter(Mandatory=$true)][string]$User,
-    [Parameter(Mandatory=$true)][string]$IP
+    [Parameter(Mandatory = $true)][string]$User,
+    [Parameter(Mandatory = $true)][string]$IP
 )
 
 Set-Location "$PSScriptRoot/.."
 
-Write-Host "Copying files to $User@$IP`:~/"
+# download passmark
+aria2c -c -x16 -s16 `
+    -d ./temp `
+    https://www.passmark.com/downloads/PerformanceTest_Linux_ARM64.zip `
+    -o passmark.zip
+7z e ./temp/passmark.zip PerformanceTest/pt_linux_arm64 -odeploy/bench
 
-# scp -r .\deploy\* "$User@$IP:~/"
+# copy file
+Write-Host "Copying files to $User@$IP`:~/"
+scp -r ./deploy/* "$User@$IP`:~/"
