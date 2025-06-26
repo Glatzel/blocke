fb81a9636a03da4cfe9090cf5da849e745bb1f25
Glatzel <893016099@qq.com>
2025-06-26 22:47:11 +0800

    f

--- scripts/download-balenaEtcher.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location $PSScriptRoot/..
-New-Item temp -ItemType Directory -ErrorAction SilentlyContinue
-gh release download -R balena-io/etcher -p *-win32-x64-*.zip -O ./temp/balenaEtcher.zip --skip-existing
-7z x ./temp/balenaEtcher.zip -obalenaEtcher
