49e1833cea55859c011619c74136184309b65c36
Glatzel <893016099@qq.com>
2025-07-01 23:09:40 +0800

    f

--- crates/rax-nmea/src/dispatcher.rs
+++ crates/rax-nmea/src/dispatcher.rs
@@ -61,6 +61,75 @@ where
         }
         (talker, identifier, sentence)
     }
+    fn process_multilines(
+        &mut self,
+        talker: Talker,
+        identifier: Identifier,
+        sentence: String,
+    ) -> Option<(Talker, Identifier, std::string::String)> {
+        let (count, idx): (usize, usize) = match identifier {
+            Identifier::GSV => {
+                let parts = sentence.split(",").collect::<Vec<&str>>();
+                let c = match parts.get(1) {
+                    Some(c) => c,
+                    None => {
+                        clerk::warn!("Sentense has no count: {}", sentence);
+                        return None;
+                    }
+                };
+                let i = match parts.get(2) {
+                    Some(c) => c,
+                    None => {
+                        clerk::warn!("Sentense has no ordinal position: {}", sentence);
+                        return None;
+                    }
+                };
+                let c: usize = match c.parse() {
+                    Ok(c) => c,
+                    Err(e) => {
+                        clerk::warn!("{}", e);
+                        return None;
+                    }
+                };
+                let i: usize = match i.parse() {
+                    Ok(i) => i,
+                    Err(e) => {
+                        clerk::warn!("{}", e);
+                        return None;
+                    }
+                };
+                (c, i)
+            }
+            //single line
+            i => panic!("Identifier `{:?}` is not a multiline nmea.", i),
+        };
+        if count == idx { todo!() } else { todo!() }
+    }
+    fn dispatch_by_lines(&mut self) -> (Talker, Identifier, std::string::String) {
+        loop {
+            let (talker, identifier, sentence) = self.preprocess();
+            match identifier {
+                // one line
+                Identifier::DHV
+                | Identifier::GGA
+                | Identifier::GLL
+                | Identifier::GSA
+                | Identifier::GST
+                | Identifier::RMC
+                | Identifier::VTG
+                | Identifier::ZDA => return (talker, identifier, sentence),
+
+                //multiline
+                Identifier::GSV => {
+                    if let Some(result) = self.process_multilines(talker, identifier, sentence) {
+                        return result;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
 }
 impl<'a, R> Iterator for Dispatcher<'a, R>
 where
@@ -68,21 +137,5 @@ where
 {
     type Item = (Talker, Identifier, std::string::String);
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let (talker, identifier, sentence) = self.preprocess();
-        match identifier {
-            // one line
-            Identifier::DHV
-            | Identifier::GGA
-            | Identifier::GLL
-            | Identifier::GSA
-            | Identifier::GST
-            | Identifier::RMC
-            | Identifier::VTG
-            | Identifier::ZDA => Some((talker, identifier, sentence)),
-
-            //multiline
-            Identifier::GSV => todo!(),
-        }
-    }
+    fn next(&mut self) -> Option<Self::Item> { Some(self.dispatch_by_lines()) }
 }
