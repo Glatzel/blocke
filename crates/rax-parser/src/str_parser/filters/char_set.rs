d45e91a6a97bfd1ab047a73596966eae0b4de2fe
Glatzel <893016099@qq.com>
2025-07-03 17:22:57 +0800

    f

--- crates/rax-parser/src/str_parser/filters/char_set.rs
+++ crates/rax-parser/src/str_parser/filters/char_set.rs
@@ -74,10 +74,8 @@ pub const ASCII_LETTERS: CharSetFilter<52> = CharSetFilter::new([
 ]);
 
 pub const ASCII_LETTERS_DIGITS: CharSetFilter<62> = CharSetFilter::new([
-    // digits
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', // uppercase letters
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', // lowercase letters
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+    'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+    'v', 'w', 'x', 'y', 'z',
 ]);
