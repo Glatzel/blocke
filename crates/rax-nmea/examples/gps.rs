9531fa0f7f774d9c2edd6cc0a986b70d2c23d0f2
Glatzel <893016099@qq.com>
2025-07-03 15:53:52 +0800

    r

--- crates/rax-nmea/examples/gps.rs
+++ crates/rax-nmea/examples/gps.rs
@@ -7,10 +7,7 @@ use rax_nmea::data::{Dhv, Gga, Gll, Gsa, Gst, Gsv, INmeaData, Identifier, Rmc, T
 use rax_parser::str_parser::StrParserContext;
 fn main() -> miette::Result<()> {
     test_utils::init_log();
-    #[cfg(target_os = "windows")]
     let path = "COM3";
-    #[cfg(target_os = "linux")]
-    let path = "/dev/ttyUSB0";
     let port = serialport::new(path, 9600)
         .timeout(Duration::from_millis(3000))
         .open()
