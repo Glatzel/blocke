22d3b866de166caacff5401857748430273184e6
Glatzel <893016099@qq.com>
2025-07-08 21:16:42 +0800

    f

--- crates/rax-nmea/src/data/gbs.rs
+++ crates/rax-nmea/src/data/gbs.rs
@@ -2,9 +2,9 @@ use std::fmt::Debug;
 
 use rax::str_parser::{ParseOptExt, StrParserContext};
 
-use crate::data::Talker;
+use crate::data::{SystemId, Talker};
 use crate::macros::readonly_struct;
-use crate::{CHAR_COMMA, NMEA_UTC, UNTIL_COMMA, UNTIL_STAR};
+use crate::{CHAR_COMMA, NMEA_UTC, UNTIL_COMMA, UNTIL_COMMA_OR_STAR, UNTIL_STAR};
 
 readonly_struct!(
     Gbs,
@@ -15,65 +15,73 @@ readonly_struct!(
    {talker: Talker},
 
    {
-       utc_time:  Option<chrono::DateTime<chrono::Utc>>,
-       "UTC time of the GGA or GNS fix associated with this sentence."
+       time:  Option<chrono::DateTime<chrono::Utc>>,
+       "UTC time to which this RAIM sentence belongs. See section UTC representation in the integration manual for details."
    },
    {
-       latitude_error:Option<f64>,
+       err_lat:Option<f64>,
        "Expected 1-sigma error in latitude (meters)"
    },
    {
-       longitude_error:Option<f64>,
+       err_lon:Option<f64>,
        "Expected 1-sigma error in longitude (meters)"
    },
    {
-       altitude_error:Option<f64>,
+       err_alt:Option<f64>,
        "Expected 1-sigma error in altitude (meters)"
    },
    {
-       likely_failed_satellite_id:Option<u16>,
-       "ID of most likely failed satellite (1 to 138)"
+       svid:Option<u16>,
+       "Satellite ID of most likely failed satellite."
    },
    {
-       missed_detection_probability:Option<f64>,
-       "Probability of missed detection for most likely failed satellite"
+       prob:Option<f64>,
+       "Probability of missed detection."
    },
    {
-       bias_estimate:Option<f64>,
-       "Estimate of bias in meters on most likely failed satellite"
+       bias:Option<f64>,
+       " Estimated bias of most likely failed satellite (a priori residual)"
    },
    {
        std_dev:Option<f64>,
        "Standard deviation of bias estimate"
+   },
+   {
+    system_id:Option<SystemId>
+   },
+   {
+    signal_id:Option<u16>
    }
 );
 
 impl Gbs {
     pub fn new(ctx: &mut StrParserContext, talker: Talker) -> miette::Result<Self> {
-        let utc_time = ctx
+        let time = ctx
             .skip_strict(&UNTIL_COMMA)?
             .skip_strict(&CHAR_COMMA)?
             .take(&NMEA_UTC);
-        let latitude_error = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
-        let longitude_error = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
-        let altitude_error = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
-        let likely_failed_satellite_id =
-            ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
-        let missed_detection_probability =
-            ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
-        let bias_estimate = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
-        let std_dev = ctx.skip(&CHAR_COMMA).take(&UNTIL_STAR).parse_opt();
+        let err_lat = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
+        let err_lon = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
+        let err_alt = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
+        let svid = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
+        let prob = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
+        let bias = ctx.skip_strict(&CHAR_COMMA)?.take(&UNTIL_COMMA).parse_opt();
+        let std_dev = ctx.skip(&CHAR_COMMA).take(&UNTIL_COMMA_OR_STAR).parse_opt();
+        let system_id = ctx.skip(&CHAR_COMMA).take(&UNTIL_COMMA_OR_STAR).parse_opt();
+        let signal_id = ctx.skip(&CHAR_COMMA).take(&UNTIL_STAR).parse_opt();
 
         Ok(Gbs {
             talker,
-            utc_time,
-            latitude_error,
-            longitude_error,
-            altitude_error,
-            likely_failed_satellite_id,
-            missed_detection_probability,
-            bias_estimate,
+            time,
+            err_lat,
+            err_lon,
+            err_alt,
+            svid,
+            prob,
+            bias,
             std_dev,
+            system_id,
+            signal_id,
         })
     }
 }
@@ -82,32 +90,35 @@ impl Debug for Gbs {
         let mut ds = f.debug_struct("GBS");
         ds.field("talker", &self.talker);
 
-        if let Some(ref utc_time) = self.utc_time {
-            ds.field("utc_time", utc_time);
+        if let Some(ref time) = self.time {
+            ds.field("time", time);
         }
-        if let Some(latitude_error) = self.latitude_error {
-            ds.field("latitude_error", &latitude_error);
+        if let Some(err_lat) = self.err_lat {
+            ds.field("err_lat", &err_lat);
         }
-        if let Some(longitude_error) = self.longitude_error {
-            ds.field("longitude_error", &longitude_error);
+        if let Some(err_lon) = self.err_lon {
+            ds.field("err_lon", &err_lon);
         }
-        if let Some(altitude_error) = self.altitude_error {
-            ds.field("altitude_error", &altitude_error);
+        if let Some(err_alt) = self.err_alt {
+            ds.field("err_alt", &err_alt);
         }
-        if let Some(likely_failed_satellite_id) = self.likely_failed_satellite_id {
-            ds.field("likely_failed_satellite_id", &likely_failed_satellite_id);
+        if let Some(svid) = self.svid {
+            ds.field("svid", &svid);
         }
-        if let Some(missed_detection_probability) = self.missed_detection_probability {
-            ds.field(
-                "missed_detection_probability",
-                &missed_detection_probability,
-            );
+        if let Some(prob) = self.prob {
+            ds.field("prob", &prob);
         }
-        if let Some(bias_estimate) = self.bias_estimate {
-            ds.field("bias_estimate", &bias_estimate);
+        if let Some(bias) = self.bias {
+            ds.field("bias", &bias);
         }
         if let Some(std_dev) = self.std_dev {
             ds.field("std_dev", &std_dev);
+        }
+        if let Some(system_id) = self.system_id {
+            ds.field("system_id", &system_id);
+        }
+        if let Some(signal_id) = self.signal_id {
+            ds.field("signal_id", &signal_id);
         }
 
         ds.finish()
@@ -127,10 +138,28 @@ mod tests {
         let mut ctx = StrParserContext::new();
         let gbs = Gbs::new(ctx.init(s.to_string()), Talker::GP).unwrap();
         println!("{gbs:?}");
-        assert_eq!(gbs.talker, Talker::GP);
-        assert!(gbs.utc_time.unwrap().to_string().contains("12:50:27"));
-        assert_eq!(gbs.latitude_error.unwrap(), 23.43);
-        assert_eq!(gbs.altitude_error.unwrap(), 13.91);
-        assert_eq!(gbs.missed_detection_probability.unwrap(), 34.01);
+        assert_eq!(gbs.talker(), &Talker::GP);
+        assert!(gbs.time().unwrap().to_string().contains("12:50:27"));
+        assert_eq!(gbs.err_lat().unwrap(), 23.43);
+        assert_eq!(gbs.err_alt().unwrap(), 13.91);
+        assert_eq!(gbs.prob().unwrap(), 34.01);
+    }
+    #[test]
+    fn test_gbs_4_1() {
+        init_log_with_level(LevelFilter::TRACE);
+        let s = "$GPGBS,235458.00,1.4,1.3,3.1,03,,-21.4,3.8,1,0*5B";
+        let mut ctx = StrParserContext::new();
+        let gbs = Gbs::new(ctx.init(s.to_string()), Talker::GP).unwrap();
+        println!("{gbs:?}");
+        assert_eq!(gbs.talker(), &Talker::GP);
+        assert!(gbs.time().unwrap().to_string().contains("23:54:58"));
+        assert_eq!(gbs.err_lat().unwrap(), 1.4);
+        assert_eq!(gbs.err_lon().unwrap(), 1.3);
+        assert_eq!(gbs.err_alt().unwrap(), 3.1);
+        assert_eq!(gbs.svid().unwrap(), 3);
+        assert_eq!(gbs.bias().unwrap(), -21.4);
+        assert_eq!(gbs.std_dev().unwrap(), 3.8);
+        assert_eq!(gbs.system_id().unwrap(), SystemId::GPS);
+        assert_eq!(gbs.signal_id().unwrap(), 0);
     }
 }
