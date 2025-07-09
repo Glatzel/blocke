d06078e5e9f1682a837aec822ffa72ce43102ea7
Glatzel <893016099@qq.com>
2025-07-09 20:45:57 +0800

    f

--- crates/rax/benches/rules.rs
+++ crates/rax/benches/rules.rs
@@ -8,77 +8,71 @@ use rax::str_parser::rules::{
     UntilNotInCharSet, UntilOneInCharSet,
 };
 
-fn bench_byte_count(c: &mut Criterion) {
-    let rule = ByteCount::<2>;
-    c.bench_function("byte_count", |b| b.iter(|| rule.apply(black_box("hello"))));
+// Helper for rules with config (structs)
+fn bench_rule<R: IStrFlowRule<'static>>(
+    c: &mut Criterion,
+    name: &str,
+    rule: R,
+    input: &'static str,
+) {
+    c.bench_function(name, |b| b.iter(|| rule.apply(black_box(input))));
 }
-fn bench_char_count(c: &mut Criterion) {
-    let rule = CharCount::<2>;
-    c.bench_function("char_count", |b| {
-        b.iter(|| rule.apply(black_box("110324,foo,bar")))
-    });
+
+fn benches(c: &mut Criterion) {
+    bench_rule(c, "byte_count", ByteCount::<2>, "hello");
+    bench_rule(c, "char_count", CharCount::<2>, "110324,foo,bar");
+    bench_rule(c, "char", Char::<'a'>, "a123");
+    bench_rule(
+        c,
+        "n_in_char_set",
+        NInCharSet::<3, 62>(&ASCII_LETTERS_DIGITS),
+        "abc123",
+    );
+    bench_rule(
+        c,
+        "one_in_char_set",
+        OneOfCharSet(&ASCII_LETTERS_DIGITS),
+        "a123",
+    );
+
+    bench_rule(
+        c,
+        "until_n_in_char_set",
+        UntilNInCharSet::<2, 10> {
+            filter: &DIGITS,
+            include: false,
+        },
+        "a1b2c3",
+    );
+    bench_rule(
+        c,
+        "until_not_in_char_set",
+        UntilNotInCharSet {
+            filter: &DIGITS,
+            include: false,
+        },
+        "123abc",
+    );
+    const FILTER: CharSetFilter<2> = CharSetFilter::<2>::new([',', '*']);
+    bench_rule(
+        c,
+        "until_one_in_char_set",
+        UntilOneInCharSet {
+            filter: &FILTER,
+            include: true,
+        },
+        "0.7,1*38",
+    );
+    bench_rule(
+        c,
+        "until",
+        Until {
+            delimiter: ";",
+            include: false,
+        },
+        "123abc",
+    );
 }
-fn bench_char(c: &mut Criterion) {
-    let rule = Char::<'a'>;
-    c.bench_function("char", |b| b.iter(|| rule.apply(black_box("a123"))));
-}
-fn bench_n_in_char_set(c: &mut Criterion) {
-    let rule = NInCharSet::<3, 62>(&ASCII_LETTERS_DIGITS);
-    c.bench_function("n_in_char_set", |b| {
-        b.iter(|| rule.apply(black_box("abc123")))
-    });
-}
-fn bench_one_in_char_set(c: &mut Criterion) {
-    let rule = OneOfCharSet(&ASCII_LETTERS_DIGITS);
-    c.bench_function("one_in_char_set", |b| {
-        b.iter(|| rule.apply(black_box("a123")))
-    });
-}
-fn bench_until_n_in_char_set(c: &mut Criterion) {
-    let rule = UntilNInCharSet::<2, 10> {
-        filter: &DIGITS,
-        include: false,
-    };
-    c.bench_function("until_n_in_char_set", |b| {
-        b.iter(|| rule.apply(black_box("a1b2c3")))
-    });
-}
-fn bench_until_not_in_char_set(c: &mut Criterion) {
-    let rule = UntilNotInCharSet {
-        filter: &DIGITS,
-        include: false,
-    };
-    c.bench_function("until_not_in_char_set", |b| {
-        b.iter(|| rule.apply(black_box("123abc")))
-    });
-}
-fn bench_until_one_in_char_set(c: &mut Criterion) {
-    let filter = CharSetFilter::<2>::from_str(",*").unwrap();
-    let rule = UntilOneInCharSet {
-        filter: &filter,
-        include: true,
-    };
-    c.bench_function("until_not_in_char_set", |b| {
-        b.iter(|| rule.apply(black_box("0.7,1*38")))
-    });
-}
-fn bench_until(c: &mut Criterion) {
-    let rule = Until {
-        delimiter: ";",
-        include: false,
-    };
-    c.bench_function("until", |b| b.iter(|| rule.apply(black_box("123abc"))));
-}
-criterion_group!(
-    benches,
-    bench_byte_count,
-    bench_char_count,
-    bench_char,
-    bench_n_in_char_set,
-    bench_one_in_char_set,
-    bench_until_n_in_char_set,
-    bench_until_not_in_char_set,
-    bench_until_one_in_char_set,
-    bench_until
-);
-criterion_main!(benches);
+
+criterion_group!(benches_group, benches);
+criterion_main!(benches_group);
