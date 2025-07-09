9f10d8b02b582dd824175fb5f639ee70cd527d69
Glatzel <893016099@qq.com>
2025-07-09 20:52:11 +0800

    f

--- crates/rax/benches/rules.rs
+++ crates/rax/benches/rules.rs
@@ -1,4 +1,3 @@
-
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use rax::str_parser::IStrFlowRule;
 use rax::str_parser::filters::{ASCII_LETTERS_DIGITS, CharSetFilter, DIGITS};
@@ -13,7 +12,13 @@ fn bench_rule<R: IStrFlowRule<'static>>(
     rule: R,
     input: &'static str,
 ) {
-    c.bench_function(name, |b| b.iter(|| rule.apply(black_box(input))));
+    c.bench_function(name, |b| {
+        b.iter(|| {
+            for _ in 0..100 {
+                rule.apply(black_box(input));
+            }
+        })
+    });
 }
 
 fn benches(c: &mut Criterion) {
