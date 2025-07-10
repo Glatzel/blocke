15bf4cb7618b048bff09ed6169e88dd0d20d41bd
Glatzel <chenzelin9794@gmail.com>
2025-07-10 15:08:49 +0800

    chore: update scripts (#61)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -3,6 +3,10 @@ param (
     [Parameter(Mandatory = $true)][string]$IP
 )
 Set-Location $PSScriptRoot/..
+
+# remove file
+ssh $User@$IP "rm -rf ~/bin ~/config ~/scripts"
+
 # copy file
 Write-Host "Copying files to $User@$IP`:~/"
 scp -r ./deploy/* "$User@$IP`:~/"
