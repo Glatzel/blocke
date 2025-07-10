1576418f942e26d76bdc68cc9d5e77be98e8f4aa
Glatzel <893016099@qq.com>
2025-07-10 15:04:42 +0800

    f

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -3,6 +3,10 @@ param (
     [Parameter(Mandatory = $true)][string]$IP
 )
 Set-Location $PSScriptRoot/..
+
+# remove file
+ssh $User@$IP "rm -rf /path/to/remote/folder"
+
 # copy file
 Write-Host "Copying files to $User@$IP`:~/"
 scp -r ./deploy/* "$User@$IP`:~/"
