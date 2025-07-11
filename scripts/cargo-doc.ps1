fc2a287d2d11696495fa88cdf78390d2b7be50db
Glatzel <893016099@qq.com>
2025-07-11 12:00:03 +0800

    fds

--- scripts/cargo-doc.ps1
+++ scripts/cargo-doc.ps1
@@ -7,8 +7,4 @@ if (Test-Path $PSScriptRoot/setup.ps1) {
 Set-Location $PSScriptRoot/..
 cargo doc --no-deps --all-features
 
-Remove-Item ./dist/rust-doc.7z -Force -ErrorAction SilentlyContinue
-New-Item ./dist -ItemType Directory -ErrorAction SilentlyContinue
-7z a -t7z -m0=LZMA2 -mmt=on -mx9 -md=4096m -mfb=273 -ms=on -mqs=on `
-    "./dist/rust-doc.7z" "./target/doc/*"
 Set-Location $ROOT
