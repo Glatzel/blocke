d3db15870a55f84edb89669964170400ee03bbeb
Glatzel <893016099@qq.com>
2025-07-11 11:59:17 +0800

    f

--- scripts/cargo-doc.ps1
+++ scripts/cargo-doc.ps1
@@ -1,6 +1,9 @@
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 $ROOT = git rev-parse --show-toplevel
+if (Test-Path $PSScriptRoot/setup.ps1) {
+    &$PSScriptRoot/setup.ps1
+}
 Set-Location $PSScriptRoot/..
 cargo doc --no-deps --all-features
 
