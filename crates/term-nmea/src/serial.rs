4e715e8b0018d770ef84f54e85252d381c9def3b
Glatzel <893016099@qq.com>
2025-07-17 19:37:00 +0800

    f

--- crates/term-nmea/src/serial.rs
+++ crates/term-nmea/src/serial.rs
@@ -5,21 +5,7 @@ use rax_nmea::data::{Identifier, Talker};
 use tokio::io::BufReader;
 use tokio::sync::mpsc::Sender;
 use tokio_serial::SerialPortBuilderExt;
-async fn run_serial_dispatcher<R: AsyncIRaxReader>(
-    mut reader: R,
-    tx: Sender<(Talker, Identifier, String)>,
-) -> miette::Result<()> {
-    let mut dispatcher = Dispatcher::new();
-    loop {
-        if let Some(msg) = reader
-            .read_line()
-            .await?
-            .and_then(|l| dispatcher.dispatch(l))
-        {
-            let _ = tx.send(msg).await;
-        }
-    }
-}
+
 pub async fn start_serial_reader(
     port: String,
     baud_rate: u32,
@@ -42,53 +28,15 @@ pub async fn start_serial_reader(
         .open_native_async()
         .into_diagnostic()
         .wrap_err_with(|| format!("Failed to open serial port: {port}"))?;
-    let reader = AsyncRaxReader::new(BufReader::new(serial));
-    run_serial_dispatcher(reader, tx).await
-}
-#[cfg(test)]
-mod tests {
-    use std::collections::VecDeque;
-
-    use async_trait::async_trait;
-    use rax::io::AsyncIRaxReader;
-    use tokio::sync::mpsc;
-
-    use super::*;
-
-    struct MockRaxReader {
-        lines: VecDeque<&'static str>,
-    }
-
-    #[async_trait]
-    impl AsyncIRaxReader for MockRaxReader {
-        async fn read_line(&mut self) -> miette::Result<Option<String>> {
-            Ok(self.lines.pop_front().map(str::to_string))
+    let mut reader = AsyncRaxReader::new(BufReader::new(serial));
+    let mut dispatcher = Dispatcher::new();
+    loop {
+        if let Some(msg) = reader
+            .read_line()
+            .await?
+            .and_then(|l| dispatcher.dispatch(l))
+        {
+            let _ = tx.send(msg).await;
         }
-        async fn read_lines_by_count(&mut self, _count: usize) -> miette::Result<Vec<String>> {
-            unimplemented!()
-        }
-    }
-
-    #[tokio::test]
-    async fn test_run_serial_dispatcher() -> miette::Result<()> {
-        let input = vec![
-            "$GPGSA,A,3,04,05,..*1C\r\n", // GSA sentence
-            "$GPGGA,1234,...*55\r\n",     // GGA sentence
-        ];
-        let reader = MockRaxReader {
-            lines: input.into_iter().collect(),
-        };
-
-        let (tx, mut rx) = mpsc::channel(10);
-        run_serial_dispatcher(reader, tx).await?;
-
-        let mut received = vec![];
-        while let Ok(msg) = rx.try_recv() {
-            received.push(msg);
-        }
-
-        assert!(received.iter().any(|(_, id, _)| *id == Identifier::GSA));
-        assert!(received.iter().any(|(_, id, _)| *id == Identifier::GGA));
-        Ok(())
     }
 }
