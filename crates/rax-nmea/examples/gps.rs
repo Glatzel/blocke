017a34dca64ed77f58530d5f4c886063956850af
Glatzel <893016099@qq.com>
2025-07-17 19:28:19 +0800

    发、

--- crates/rax-nmea/examples/gps.rs
+++ crates/rax-nmea/examples/gps.rs
@@ -17,110 +17,112 @@ fn main() -> miette::Result<()> {
     let mut reader = rax::io::RaxReader::new(BufReader::new(port));
     let mut ctx = StrParserContext::new();
     let mut dispatcher = Dispatcher::new();
-    while let Some((talker, identifier, sentence)) = reader
-        .read_line()?
-        .and_then(|line| dispatcher.dispatch(line))
-    {
-        match identifier {
-            Identifier::DHV => {
-                let ctx = ctx.init(sentence);
-                let nmea = Dhv::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GBS => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gbs::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GGA => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gga::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GLL => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gll::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GNS => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gns::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GRS => {
-                let ctx = ctx.init(sentence);
-                let nmea = Grs::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GSA => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gsa::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GST => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gst::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GSV => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gsv::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::RMC => {
-                let ctx = ctx.init(sentence);
-                let nmea = Rmc::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::TXT => {
-                let ctx = ctx.init(sentence);
-                let nmea = Txt::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::VTG => {
-                let ctx = ctx.init(sentence);
-                let nmea = Vtg::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::ZDA => {
-                let ctx = ctx.init(sentence);
-                let nmea = Zda::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::DTM => {
-                let ctx = ctx.init(sentence);
-                let nmea = Dtm::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GBQ => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gbq::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GLQ => {
-                let ctx = ctx.init(sentence);
-                let nmea = Glq::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GNQ => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gnq::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::GPQ => {
-                let ctx = ctx.init(sentence);
-                let nmea = Gpq::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::THS => {
-                let ctx = ctx.init(sentence);
-                let nmea = Ths::new(ctx, talker)?;
-                println!("{nmea:?}")
-            }
-            Identifier::VLW => {
-                let ctx = ctx.init(sentence);
-                let nmea = Vlw::new(ctx, talker)?;
-                println!("{nmea:?}")
+    loop {
+        if let Some((talker, identifier, sentence)) = reader
+            .read_line()?
+            .and_then(|line| dispatcher.dispatch(line))
+        {
+            match identifier {
+                Identifier::DHV => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Dhv::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GBS => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gbs::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GGA => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gga::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GLL => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gll::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GNS => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gns::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GRS => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Grs::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GSA => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gsa::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GST => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gst::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GSV => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gsv::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::RMC => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Rmc::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::TXT => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Txt::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::VTG => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Vtg::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::ZDA => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Zda::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::DTM => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Dtm::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GBQ => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gbq::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GLQ => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Glq::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GNQ => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gnq::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::GPQ => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Gpq::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::THS => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Ths::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
+                Identifier::VLW => {
+                    let ctx = ctx.init(sentence);
+                    let nmea = Vlw::new(ctx, talker)?;
+                    println!("{nmea:?}")
+                }
             }
         }
     }
