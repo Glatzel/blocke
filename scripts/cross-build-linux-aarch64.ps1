4bf5a517e579b2477fb531f14c0fbaf4605ecc68
Glatzel <chenzelin9794@gmail.com>
2025-07-17 17:14:56 +0800

    fix: deploy (#70)

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -3,8 +3,8 @@ Set-Location $PSScriptRoot/..
 cargo install cross
 if ($IsWindows) { rustup toolchain add stable-x86_64-unknown-linux-gnu --profile minimal --force-non-host }
 if ($Release) {
-    cross install --target aarch64-unknown-linux-gnu --all-features --root ./deploy/bin
-    cross install --target aarch64-unknown-linux-gnu --all-features --examples --root ./deploy/bin/examples
+    cross build --target aarch64-unknown-linux-gnu --all-features --release --bins
+    Copy-Item ./target/aarch64-unknown-linux-gnu/release/term-nmea ./deploy/linux-aarch64/bin/
 }
 else {
     cross build --target aarch64-unknown-linux-gnu --all-features
