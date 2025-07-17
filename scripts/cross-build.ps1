725727cb74fd00a8ab9717151ab17235b424f218
Glatzel <893016099@qq.com>
2025-07-17 16:32:05 +0800

    f

--- scripts/cross-build.ps1
+++ scripts/cross-build.ps1
@@ -3,9 +3,8 @@ Set-Location $PSScriptRoot/..
 cargo install cross
 if ($IsWindows) { rustup toolchain add stable-x86_64-unknown-linux-gnu --profile minimal --force-non-host }
 if ($Release) {
-    cross build --target aarch64-unknown-linux-gnu --all-features --release
-    cross build --target aarch64-unknown-linux-gnu --all-features --examples --release
-
+    cross install --target aarch64-unknown-linux-gnu --all-features --root ./deploy/bin
+    cross install --target aarch64-unknown-linux-gnu --all-features --examples --root ./deploy/bin/examples
 }
 else {
     cross build --target aarch64-unknown-linux-gnu --all-features
