1b30bfc323a252a1f75b97e0832545468b39274e
Glatzel <893016099@qq.com>
2025-07-18 10:15:55 +0800

    f

--- crates/term-nmea/src/tab/nmea.rs
+++ crates/term-nmea/src/tab/nmea.rs
@@ -1,53 +1,16 @@
 use std::collections::VecDeque;
 
-use crossterm::event::{KeyCode, KeyEvent, KeyEventKind, MouseEventKind};
+use crossterm::event::KeyEvent;
 use ratatui::text::Line;
-use ratatui::widgets::{Paragraph, Scrollbar, ScrollbarOrientation, ScrollbarState};
+use ratatui::widgets::Paragraph;
 use rax_nmea::data::{Identifier, Talker};
 
-pub struct TabNmea {
-    pub lock_to_bottom: bool,
-    pub vertical_scroll_state: ScrollbarState,
-    pub vertical_scroll: usize,
-}
-impl Default for TabNmea {
-    fn default() -> Self {
-        Self {
-            lock_to_bottom: true,
-            vertical_scroll_state: Default::default(),
-            vertical_scroll: Default::default(),
-        }
-    }
-}
+#[derive(Default)]
+pub struct TabNmea {}
+
 impl super::ITab for TabNmea {
-    fn handle_key(&mut self, key: KeyEvent) {
-        if key.kind != KeyEventKind::Press {
-            return;
-        }
-        match key.code {
-            KeyCode::Down => {
-                self.vertical_scroll = self.vertical_scroll.saturating_add(1);
-            }
-            KeyCode::Up => {
-                self.lock_to_bottom = false;
-                self.vertical_scroll = self.vertical_scroll.saturating_sub(1);
-            }
-            KeyCode::Char('b') => self.lock_to_bottom = true,
-            _ => {}
-        }
-    }
-    fn handle_mouse(&mut self, mouse: crossterm::event::MouseEvent) {
-        match mouse.kind {
-            MouseEventKind::ScrollDown => {
-                self.vertical_scroll = self.vertical_scroll.saturating_add(1);
-            }
-            MouseEventKind::ScrollUp => {
-                self.lock_to_bottom = false;
-                self.vertical_scroll = self.vertical_scroll.saturating_sub(1);
-            }
-            _ => {}
-        }
-    }
+    fn handle_key(&mut self, _key: KeyEvent) {}
+    fn handle_mouse(&mut self, _mouse: crossterm::event::MouseEvent) {}
     fn draw(
         &mut self,
         f: &mut ratatui::Frame,
@@ -57,30 +20,16 @@ impl super::ITab for TabNmea {
         let count = raw_nmea.len();
 
         let visible_lines = area.height as usize;
-        if self.vertical_scroll + visible_lines >= count || self.lock_to_bottom {
-            self.lock_to_bottom = true;
-            self.vertical_scroll = count.saturating_sub(visible_lines);
-        }
-        self.vertical_scroll_state = self.vertical_scroll_state.content_length(count);
-        self.vertical_scroll_state = self.vertical_scroll_state.position(self.vertical_scroll);
 
         let p = Paragraph::new(
             raw_nmea
                 .iter()
-                .skip(self.vertical_scroll)
-                .take(self.vertical_scroll + visible_lines)
+                .skip(count - visible_lines)
                 .map(|f| Line::from(f.2.as_str()))
                 .collect::<Vec<Line>>(),
         );
 
         f.render_widget(p, area);
-        f.render_stateful_widget(
-            Scrollbar::new(ScrollbarOrientation::VerticalRight)
-                .begin_symbol(Some("↑"))
-                .end_symbol(Some("↓")),
-            area,
-            &mut self.vertical_scroll_state,
-        );
     }
 
     fn hint(&mut self) -> &'static [&'static str] { &["`b` Lock to Bottom", "`↑↓` Scroll"] }
