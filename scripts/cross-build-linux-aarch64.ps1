0b1d2ac5c6633b9f3e6c5ed8bd7428f8e088a60e
Glatzel <893016099@qq.com>
2025-07-20 04:36:32 +0800

    更新 cross-build-linux-aarch64.ps1

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -1,12 +1,20 @@
 param([switch]$Release)
 Set-Location $PSScriptRoot/..
-cargo install cross
+rustup target add aarch64-unknown-linux-gnu
+sudo dpkg --add-architecture arm64
+    sudo apt-get update
+    sudo apt-get install -y `
+      gcc-aarch64-linux-gnu `
+      g++-aarch64-linux-gnu `
+      libproj-dev:arm64 `
+      pkg-config:arm64 `
+      qemu-user-static
 if ($IsWindows) { rustup toolchain add stable-x86_64-unknown-linux-gnu --profile minimal --force-non-host }
 if ($Release) {
     cross build --target aarch64-unknown-linux-gnu --all-features --release --bins
     Copy-Item ./target/aarch64-unknown-linux-gnu/release/term-nmea ./deploy/linux-aarch64/bin/
 }
 else {
-    cross build --target aarch64-unknown-linux-gnu --all-features
-    cross build --target aarch64-unknown-linux-gnu --all-features --examples
+    cargo build --target aarch64-unknown-linux-gnu --all-features
+    cargo build --target aarch64-unknown-linux-gnu --all-features --examples
 }
