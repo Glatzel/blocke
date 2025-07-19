5d6ce4ecc61b44ecdd8d54651a760dfb044fb8c0
Glatzel <893016099@qq.com>
2025-07-19 19:06:03 +0800

    更新 cross-build-linux-aarch64.ps1

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -1,6 +1,6 @@
 param([switch]$Release)
 Set-Location $PSScriptRoot/..
-
+pixi install
 cargo install cross
 if ($IsWindows) { rustup toolchain add stable-x86_64-unknown-linux-gnu --profile minimal --force-non-host }
 if ($Release) {
