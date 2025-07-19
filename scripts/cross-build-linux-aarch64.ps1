8c4e52ca446750e49a53215754bb2387e0beb0d1
Glatzel <893016099@qq.com>
2025-07-19 15:18:15 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -1,12 +1,13 @@
 param([switch]$Release)
 Set-Location $PSScriptRoot/..
 cargo install cross
+$env:PKG_CONFIG_PATH = ./.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
 if ($IsWindows) { rustup toolchain add stable-x86_64-unknown-linux-gnu --profile minimal --force-non-host }
 if ($Release) {
-    cross build --target aarch64-unknown-linux-gnu --all-features --release --bins --env PKG_CONFIG_PATH=./.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
+    cross build --target aarch64-unknown-linux-gnu --all-features --release --bins
     Copy-Item ./target/aarch64-unknown-linux-gnu/release/term-nmea ./deploy/linux-aarch64/bin/
 }
 else {
-    cross build --target aarch64-unknown-linux-gnu --all-features  --env PKG_CONFIG_PATH=./.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
-    cross build --target aarch64-unknown-linux-gnu --all-features --examples --env PKG_CONFIG_PATH=./.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
+    cross build --target aarch64-unknown-linux-gnu --all-features
+    cross build --target aarch64-unknown-linux-gnu --all-features --examples
 }
