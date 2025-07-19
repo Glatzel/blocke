34d6cbf2a7139b3fb73f59f2760fb196df5e75c6
Glatzel <893016099@qq.com>
2025-07-19 16:06:31 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -1,6 +1,14 @@
 param([switch]$Release)
 Set-Location $PSScriptRoot/..
-cargo install cross
+# download proj
+aria2c -c -x16 -s16 `
+    -d ./temp `
+    https://repo.prefix.dev/glatzel/linux-aarch64/proj-9.6.2-he8cfe8b_2.conda `
+    -o proj.zip
+7z x ./temp/proj.zip -otemp
+7z x ./temp/pkg-proj*.tar.zst -otemp
+
+
 if ($IsWindows) { rustup toolchain add stable-x86_64-unknown-linux-gnu --profile minimal --force-non-host }
 if ($Release) {
     cross build --target aarch64-unknown-linux-gnu --all-features --release --bins
