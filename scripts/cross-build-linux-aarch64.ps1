1679f0286cd32a7643bdb3f62e231035f3eb2bea
Glatzel <893016099@qq.com>
2025-07-19 22:12:48 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -1,6 +1,6 @@
 param([switch]$Release)
 Set-Location $PSScriptRoot/..
-$env:Path="/root/.pixi/bin"+":"+"$env:Path"
+
 cargo install cross
 if ($IsWindows) { rustup toolchain add stable-x86_64-unknown-linux-gnu --profile minimal --force-non-host }
 if ($Release) {
