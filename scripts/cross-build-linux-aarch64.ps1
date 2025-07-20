ab2d9ef786b4bd61ca9bda7d6a28865f3ee90820
Glatzel <893016099@qq.com>
2025-07-20 19:00:35 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -1,18 +1,25 @@
 param([switch]$Release)
+
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
-if ($IsLinux) {
-    Set-Location $PSScriptRoot/..
-    cargo install cargo-zigbuild
-    # sudo dpkg --add-architecture arm64
-    # sudo apt-get install -y musl-dev
-    rustup target add aarch64-unknown-linux-musl
-    pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
 
-    # Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
-    $p = resolve-path ~/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig
+if ($IsLinux) {
+    Set-Location "$PSScriptRoot/.."
+
+    sudo apt update
+    sudo apt install zig
+
+    cargo install cargo-zigbuild --locked
+    rustup target add aarch64-unknown-linux-musl
+
+    # Install proj via Pixi
+    pixi install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
+
+    # Set PKG_CONFIG_PATH
+    $p = Resolve-Path "$HOME/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig"
     $env:PKG_CONFIG_PATH = "$p" + ":" + "/usr/lib/aarch64-linux-gnu/pkgconfig" + ":" + "$env:PKG_CONFIG_PATH"
     $env:PKG_CONFIG_ALLOW_CROSS = 1
+
     if ($Release) {
         cargo zigbuild --target aarch64-unknown-linux-musl --all-features --release --bins
     }
@@ -21,4 +28,7 @@ if ($IsLinux) {
         cargo zigbuild --target aarch64-unknown-linux-musl --all-features --examples
     }
 }
-else { exit 1 }
+else {
+    Write-Error "This script must be run on Linux."
+    exit 1
+}
