992dd3c3198b962423248ba78080251b29cb234c
Glatzel <893016099@qq.com>
2025-07-20 19:08:36 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -5,7 +5,7 @@ if ($IsLinux) {
     Set-Location $PSScriptRoot/..
     sudo apt-get update
     # sudo dpkg --add-architecture arm64
-    sudo apt-get install -y g++-aarch64-linux-gnu
+    sudo apt-get install -y g++-aarch64-linux-gnu musl-tools
     rustup target add aarch64-unknown-linux-musl
     pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
 
