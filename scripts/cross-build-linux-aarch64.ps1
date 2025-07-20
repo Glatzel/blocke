cd8db5e6199e91df25003236ed37b4f756a9ae90
Glatzel <893016099@qq.com>
2025-07-20 19:07:49 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -1,59 +1,24 @@
 param([switch]$Release)
-
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
-
 if ($IsLinux) {
-    # Define version and download URL
-    $ZIG_VERSION = "0.14.1"
-    $ZIG_TAR = "zig-linux-x86_64-$ZIG_VERSION.tar.xz"
-    $ZIG_DIR = "zig-linux-x86_64-$ZIG_VERSION"
-    $ZIG_URL = "https://ziglang.org/download/$ZIG_VERSION/$ZIG_TAR"
-
-    # Download Zig
-    if (-not (Test-Path $ZIG_TAR)) {
-        Write-Host "Downloading Zig $ZIG_VERSION..."
-        Invoke-WebRequest -Uri $ZIG_URL -OutFile $ZIG_TAR
-    }
-
-    # Extract Zig
-    if (-not (Test-Path $ZIG_DIR)) {
-        Write-Host "Extracting Zig..."
-        tar -xf $ZIG_TAR
-    }
-
-    # Add to PATH temporarily (for this session)
-    $zigFullPath = Resolve-Path "./$ZIG_DIR"
-    $env:PATH = "$zigFullPath" + ":" + $env:PATH
-
-    # Confirm installation
-    zig version
-
-    Set-Location "$PSScriptRoot/.."
-
-    sudo apt update
-    sudo apt install zig
-
-    cargo install cargo-zigbuild --locked
+    Set-Location $PSScriptRoot/..
+    sudo apt-get update
+    # sudo dpkg --add-architecture arm64
+    sudo apt-get install -y g++-aarch64-linux-gnu
     rustup target add aarch64-unknown-linux-musl
+    pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
 
-    # Install proj via Pixi
-    pixi install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
-
-    # Set PKG_CONFIG_PATH
-    $p = Resolve-Path "$HOME/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig"
+    # Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
+    $p = resolve-path ~/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig
     $env:PKG_CONFIG_PATH = "$p" + ":" + "/usr/lib/aarch64-linux-gnu/pkgconfig" + ":" + "$env:PKG_CONFIG_PATH"
     $env:PKG_CONFIG_ALLOW_CROSS = 1
-
     if ($Release) {
-        cargo zigbuild --target aarch64-unknown-linux-musl --all-features --release --bins
+        cargo build --target aarch64-unknown-linux-musl --all-features --release --bins
     }
     else {
-        cargo zigbuild --target aarch64-unknown-linux-musl --all-features
-        cargo zigbuild --target aarch64-unknown-linux-musl --all-features --examples
+        cargo build --target aarch64-unknown-linux-musl --all-features
+        cargo build --target aarch64-unknown-linux-musl --all-features --examples
     }
 }
-else {
-    Write-Error "This script must be run on Linux."
-    exit 1
-}
+else { exit 1 }
