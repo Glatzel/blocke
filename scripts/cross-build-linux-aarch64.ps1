537400bd6006b353edd15857ba0f110739750798
Glatzel <893016099@qq.com>
2025-07-20 14:33:58 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -7,7 +7,7 @@ if ($IsLinux) {
     sudo dpkg --add-architecture arm64
     sudo apt-get install -y `
         g++-aarch64-linux-gnu `
-        librust-libudev-dev:arm64
+        rust-libudev:arm64
     rustup target add aarch64-unknown-linux-gnu
     pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
 
