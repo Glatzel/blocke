028fd46b45a90afae1c9d855ca51096740538e0f
Glatzel <chenzelin9794@gmail.com>
2025-07-20 15:53:38 +0800

    build: proj aarch64 (#78)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -1,12 +1,26 @@
 param([switch]$Release)
-Set-Location $PSScriptRoot/..
-cargo install cross
-if ($IsWindows) { rustup toolchain add stable-x86_64-unknown-linux-gnu --profile minimal --force-non-host }
-if ($Release) {
-    cross build --target aarch64-unknown-linux-gnu --all-features --release --bins
-    Copy-Item ./target/aarch64-unknown-linux-gnu/release/term-nmea ./deploy/linux-aarch64/bin/
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+if ($IsLinux) {
+    Set-Location $PSScriptRoot/..
+    cargo tree
+    sudo apt-get update
+    sudo dpkg --add-architecture arm64
+    sudo apt-get install -y g++-aarch64-linux-gnu
+    rustup target add aarch64-unknown-linux-gnu
+    pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
+
+    # Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
+    $p = resolve-path ~/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig
+    $env:PKG_CONFIG_PATH = "$p" + ":" + "/usr/lib/aarch64-linux-gnu/pkgconfig" + ":" + "$env:PKG_CONFIG_PATH"
+    $env:PKG_CONFIG_ALLOW_CROSS = 1
+    if ($Release) {
+        cargo build --target aarch64-unknown-linux-gnu --all-features --release --bins
+        Copy-Item ./target/aarch64-unknown-linux-gnu/release/term-nmea ./deploy/linux-aarch64/bin/
+    }
+    else {
+        cargo build --target aarch64-unknown-linux-gnu --all-features
+        cargo build --target aarch64-unknown-linux-gnu --all-features --examples
+    }
 }
-else {
-    cross build --target aarch64-unknown-linux-gnu --all-features
-    cross build --target aarch64-unknown-linux-gnu --all-features --examples
-}
+else { exit 1 }
