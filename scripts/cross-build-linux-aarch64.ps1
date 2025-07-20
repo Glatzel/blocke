e76dbd967422d1d62843264cf7a1bb4595647921
Glatzel <893016099@qq.com>
2025-07-20 19:34:00 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -4,14 +4,13 @@ $PSNativeCommandUseErrorActionPreference = $true
 if ($IsLinux) {
     Set-Location $PSScriptRoot/..
     sudo apt-get update
-    # sudo dpkg --add-architecture arm64
     sudo apt-get install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
     rustup target add aarch64-unknown-linux-musl
     pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
 
     # Set PKG_CONFIG_PATH to vcpkg's pkgconfig directory
     $p = resolve-path ~/.pixi/envs/proj/proj/arm64-linux-release/lib/pkgconfig
-    $env:PKG_CONFIG_PATH = "$p" + ":" + "/usr/lib/aarch64-linux-gnu/pkgconfig" + ":" + "$env:PKG_CONFIG_PATH"
+    $env:PKG_CONFIG_PATH = "$p" + ":" + "$env:PKG_CONFIG_PATH"
     $env:PKG_CONFIG_ALLOW_CROSS = 1
     if ($Release) {
         cargo build --target aarch64-unknown-linux-musl --all-features --release --bins
