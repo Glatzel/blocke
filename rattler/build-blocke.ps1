012065c52b1996eee4b1f49eab69b25bdd7449dc
Glatzel <893016099@qq.com>
2025-07-20 17:00:58 +0800

    f

--- rattler/build-blocke.ps1
+++ rattler/build-blocke.ps1
@@ -1,6 +1,10 @@
 Set-Location $PSScriptRoot
 $ROOT = git rev-parse --show-toplevel
-. $ROOT/scripts/util.ps1
 
 New-Item $env:PREFIX/bin -ItemType Directory
-Copy-Item "$ROOT/temp/$name/bin/*" "$env:PREFIX/bin/"
+if ($IsWindows) {
+    Copy-Item "$ROOT/dist/term-nmea.exe" "$env:PREFIX/bin/"
+}
+else {
+    Copy-Item "$ROOT/dist/term-nmea" "$env:PREFIX/bin/"
+}
