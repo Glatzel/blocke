cab138eb47a5d5239840390280894c072118d8bd
Glatzel <893016099@qq.com>
2025-07-20 18:57:00 +0800

    f

--- scripts/cross-build-linux-aarch64.ps1
+++ scripts/cross-build-linux-aarch64.ps1
@@ -3,9 +3,9 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 if ($IsLinux) {
     Set-Location $PSScriptRoot/..
-    sudo apt-get update
+    cargo install cargo-zigbuild
     # sudo dpkg --add-architecture arm64
-    sudo apt-get install -y musl-dev
+    # sudo apt-get install -y musl-dev
     rustup target add aarch64-unknown-linux-musl
     pixi global install proj -c https://repo.prefix.dev/glatzel --platform linux-aarch64
 
@@ -14,11 +14,11 @@ if ($IsLinux) {
     $env:PKG_CONFIG_PATH = "$p" + ":" + "/usr/lib/aarch64-linux-gnu/pkgconfig" + ":" + "$env:PKG_CONFIG_PATH"
     $env:PKG_CONFIG_ALLOW_CROSS = 1
     if ($Release) {
-        cargo build --target aarch64-unknown-linux-musl --all-features --release --bins
+        cargo zigbuild --target aarch64-unknown-linux-musl --all-features --release --bins
     }
     else {
-        cargo build --target aarch64-unknown-linux-musl --all-features
-        cargo build --target aarch64-unknown-linux-musl --all-features --examples
+        cargo zigbuild --target aarch64-unknown-linux-musl --all-features
+        cargo zigbuild --target aarch64-unknown-linux-musl --all-features --examples
     }
 }
 else { exit 1 }
