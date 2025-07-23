e07c1d44fd134e8e186041335e45f57d14c0d494
Glatzel <893016099@qq.com>
2025-07-23 15:38:43 +0800

    更新 setup.ps1

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -10,8 +10,10 @@ if ($IsMacOS) {
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/lib/pkgconfig
 }
 if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
+    sudo apk install -y libudev-dev libc6-dev
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
 }
 if ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
+    sudo apk install -y libudev-dev libc6-dev
     $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
 }
