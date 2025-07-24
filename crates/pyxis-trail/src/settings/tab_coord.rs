5b6bc57331464f8ddf89902d345c67fa801228b7
Glatzel <893016099@qq.com>
2025-07-24 11:48:41 +0800

    f

--- crates/pyxis-trail/src/settings/tab_coord.rs
+++
@@ -1,6 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct TabCoordSettings {
-    pub custom_cs: String,
-}
--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,19 +1,12 @@
 Set-Location $PSScriptRoot/..
-pixi install
 
 if ($IsWindows) {
-    $bin = Resolve-Path $PSScriptRoot/../.pixi/envs/default/Library/bin
-    $env:Path = "$bin" + ";" + "$env:Path"
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-windows-static/lib/pkgconfig
 }
 if ($IsMacOS) {
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-osx-release/lib/pkgconfig
 }
 if ($IsLinux -and ($(uname -m) -eq 'x86_64' )) {
     sudo apt install -y libudev-dev libc6-dev
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/x64-linux-release/lib/pkgconfig
 }
 if ($IsLinux -and ($(uname -m) -eq 'aarch64' )) {
     sudo apt install -y libudev-dev libc6-dev
-    $env:PKG_CONFIG_PATH = Resolve-Path $PSScriptRoot/../.pixi/envs/default/proj/arm64-linux-release/lib/pkgconfig
 }
