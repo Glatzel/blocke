0437d46a5307ab619c2b12bb02b073e73849c7d7
Glatzel <chenzelin9794@gmail.com>
2025-08-18 18:06:16 +0800

    chore: update configuration files synced file(s) with Glatzel/template (#100)
    synced local file(s) with
    [Glatzel/template](https://github.com/Glatzel/template).
    
    
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local directory <code>./pre-commit/</code> with remote
    directory <code>./pre-commit/</code></li><li>created local
    <code>./cliff.toml</code> from remote
    <code>./common/cliff.toml</code></li><li>created local directory
    <code>./esp32-c3/scripts/</code> and copied all sub files/folders from
    remote directory <code>./rust/scripts/</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#17037449744](https://github.com/Glatzel/template/actions/runs/17037449744)

---
+++ esp32-c3/scripts/cargo-test.ps1
@@ -0,0 +1,39 @@
+# This File is automatically synchronized from https://github.com/Glatzel/template
+
+if (Test-Path $PSScriptRoot/setup.ps1) {
+    &$PSScriptRoot/setup.ps1
+}
+$ROOT = git rev-parse --show-toplevel
+Set-Location $PSScriptRoot/..
+
+Write-Output "::group::nextest"
+cargo +nightly llvm-cov nextest --no-report --all --all-features --branch --no-fail-fast
+$code = $LASTEXITCODE
+Write-Output "::endgroup::"
+
+Write-Output "::group::doctest"
+cargo +nightly llvm-cov --no-report --all --all-features --branch --no-fail-fast --doc
+$code = $code + $LASTEXITCODE
+Write-Output "::endgroup::"
+
+Write-Output "::group::report"
+if ( $env:CI ) {
+    cargo +nightly llvm-cov report --lcov --output-path lcov.info
+}
+else {
+    cargo +nightly llvm-cov report
+    cargo +nightly llvm-cov report --html
+}
+Write-Output "::endgroup::"
+
+Write-Output "::group::result"
+$code = $code + $LASTEXITCODE
+if ($code -ne 0) {
+    Write-Host "Test failed." -ForegroundColor Red
+}
+else {
+    Write-Host "Test succeeded." -ForegroundColor Green
+}
+Write-Output "::endgroup::"
+Set-Location $ROOT
+exit $code
