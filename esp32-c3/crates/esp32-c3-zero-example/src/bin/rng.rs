ee69ea62ca0fd4ada4c5c9540d482cd9c81172d0
Glatzel <chenzelin9794@gmail.com>
2025-08-18 17:12:19 +0800

    feat: add rng example (#99)

---
+++ esp32-c3/crates/esp32-c3-zero-example/src/bin/rng.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::delay::Delay;
+use esp_hal::main;
+use esp_hal::rng::Rng;
+use esp_println::println;
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    let peripherals = esp_hal::init(Default::default());
+    let mut rng = Rng::new(peripherals.RNG);
+    let delay = Delay::new();
+    loop {
+        let num: u32 = rng.random();
+        delay.delay_millis(1000);
+        println!("RNG: {num}");
+    }
+}
