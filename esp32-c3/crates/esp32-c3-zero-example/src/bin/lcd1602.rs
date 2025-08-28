7e66986757652b561bcd4a610583d7d62ebd3a9a
Glatzel <chenzelin9794@gmail.com>
2025-08-28 23:05:00 +0800

    feat: lcd 1602 (#113)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

---
+++ esp32-c3/crates/esp32-c3-zero-example/src/bin/lcd1602.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::i2c::master::{Config, I2c};
+use esp_hal::main;
+use i2c_character_display::{CharacterDisplayPCF8574T, LcdDisplayType};
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let delay = Delay::new();
+    let _oe = Output::new(peripherals.GPIO2, Level::High, OutputConfig::default());
+
+    let i2c = I2c::new(peripherals.I2C0, Config::default())
+        .unwrap()
+        .with_sda(peripherals.GPIO0)
+        .with_scl(peripherals.GPIO1);
+
+    // PCF8574T adapter for a single HD44780 controller
+    let mut lcd = CharacterDisplayPCF8574T::new(i2c, LcdDisplayType::Lcd16x2, delay);
+    if let Err(e) = lcd.init() {
+        panic!("Error initializing LCD: {}", e);
+    };
+    lcd.backlight(true).unwrap().print("Hello World!").unwrap();
+    loop {}
+}
