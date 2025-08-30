1b4b902f2329114ee0d7708e2426134ff493a6c8
Glatzel <893016099@qq.com>
2025-08-30 14:48:38 +0800

    f

--- esp32-c3/crates/esp32-c3-zero-example/src/bin/blinky_led5050.rs
+++ esp32-c3/crates/esp32-c3-zero-example/src/bin/blinky_led5050.rs
@@ -3,8 +3,7 @@
 use esp_hal::delay::Delay;
 use esp_hal::gpio::{Level, Output, OutputConfig};
 use esp_hal::main;
-use mischief as _;
-use esp_alloc as _;
+use {esp_alloc as _, mischief as _};
 esp_bootloader_esp_idf::esp_app_desc!();
 #[main]
 fn main() -> ! {
