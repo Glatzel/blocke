ffa38a8c58b5c4f0f74c7730fdbd6dc98da8cec8
Glatzel <chenzelin9794@gmail.com>
2025-08-30 23:38:55 +0800

    perf: avoids repeatedly allocating and deallocating the string buffers (#120)

--- esp32-c3/crates/hygrothermograph/src/main.rs
+++ esp32-c3/crates/hygrothermograph/src/main.rs
@@ -59,6 +59,11 @@ fn app() -> mischief::Result<()> {
         .into_mischief()?;
     delay.delay_millis(2000);
 
+    // variables in loop
+    let mut temperature: f32;
+    let mut humidity: f32;
+    let mut buf_temp: String<16> = String::new();
+    let mut buf_humid: String<16> = String::new();
     loop {
         lcd.clear()
             .map_err(|_| "")
@@ -66,14 +71,14 @@ fn app() -> mischief::Result<()> {
             .home()
             .map_err(|_| "")
             .into_mischief()?;
-        let measurement = sht40.measure(Precision::Low, &mut delay);
-        if let Ok(measurement) = measurement {
-            let temperature: f32 = measurement.temperature_celsius().to_num();
-            let humidity: f32 = measurement.humidity_percent().to_num();
 
-            let mut buf_temp: String<16> = String::new();
+        if let Ok(measurement) = sht40.measure(Precision::Low, &mut delay) {
+            temperature = measurement.temperature_celsius().to_num();
+            humidity = measurement.humidity_percent().to_num();
+
+            buf_temp.clear();
+            buf_humid.clear();
             write!(buf_temp, "Temp: {:.2}C", temperature).into_mischief()?;
-            let mut buf_humid: String<16> = String::new();
             write!(buf_humid, "Hum:  {:.2}%", humidity).into_mischief()?;
             lcd.write_str(&buf_temp).into_mischief()?;
 
