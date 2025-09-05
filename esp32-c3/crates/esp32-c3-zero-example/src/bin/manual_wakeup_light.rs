c553b1dc7c020b67f1d964025e76732828510bcd
Glatzel <chenzelin9794@gmail.com>
2025-09-05 16:39:37 +0800

    feat: add wakeup switch to hygrothermogrtaph (#129)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- esp32-c3/crates/esp32-c3-zero-example/src/bin/manual_wakeup_light.rs
+++ esp32-c3/crates/esp32-c3-zero-example/src/bin/manual_wakeup_light.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+
 use esp_alloc::heap_allocator;
 use esp_hal::delay::Delay;
 use esp_hal::gpio::{Input, InputConfig, Pull, WakeEvent};
--- esp32-c3/crates/hygrothermograph/src/main.rs
+++ esp32-c3/crates/hygrothermograph/src/main.rs
@@ -7,15 +7,23 @@ use core::fmt::Write;
 use embedded_hal_bus::i2c as i2c_bus;
 use esp_alloc::heap_allocator;
 use esp_hal::delay::Delay;
-use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull, WakeEvent};
 use esp_hal::i2c::master::{Config, I2c};
-use esp_hal::main;
+use esp_hal::rtc_cntl::sleep::WakeSource;
+use esp_hal::{main, rtc_cntl};
 use heapless::String;
 use i2c_character_display::{CharacterDisplayPCF8574T, LcdDisplayType};
 use sht4x::{Precision, Sht4x};
 use {esp_alloc as _, pain as _};
 esp_bootloader_esp_idf::esp_app_desc!();
 
+macro_rules! config_dangling_pin {
+    ($pin:expr) => {
+        let config = InputConfig::default().with_pull(Pull::Down);
+        let mut wakeup_pin = Input::new($pin, config);
+        wakeup_pin.wakeup_enable(false, WakeEvent::LowLevel)?;
+    };
+}
 #[main]
 fn main() -> ! {
     heap_allocator!(size:64 * 1024);
@@ -38,6 +46,25 @@ fn app() -> mischief::Result<()> {
         .with_scl(peripherals.GPIO1);
     let i2c_ref_cell = RefCell::new(i2c0);
 
+    // config wakeup pin
+    let config = InputConfig::default().with_pull(Pull::Up);
+    let mut wakeup_pin = Input::new(peripherals.GPIO9, config);
+    wakeup_pin.wakeup_enable(true, WakeEvent::LowLevel)?;
+    config_dangling_pin!(peripherals.GPIO3);
+    config_dangling_pin!(peripherals.GPIO4);
+    config_dangling_pin!(peripherals.GPIO5);
+    config_dangling_pin!(peripherals.GPIO6);
+    config_dangling_pin!(peripherals.GPIO7);
+    config_dangling_pin!(peripherals.GPIO8);
+
+    //init rtc
+    let mut rtc = rtc_cntl::Rtc::new(peripherals.LPWR);
+    let wakeup_source = esp_hal::rtc_cntl::sleep::GpioWakeupSource::new();
+    let mut sleep_config = rtc_cntl::sleep::RtcSleepConfig::default();
+    let mut trigger = rtc_cntl::sleep::WakeTriggers::default();
+    trigger.set_gpio(true);
+    wakeup_source.apply(&rtc, &mut trigger, &mut sleep_config);
+
     // init lcd1602
     let mut lcd = CharacterDisplayPCF8574T::new(
         i2c_bus::RefCellDevice::new(&i2c_ref_cell),
@@ -50,24 +77,26 @@ fn app() -> mischief::Result<()> {
     let mut sht40 = Sht4x::new(i2c_bus::RefCellDevice::new(&i2c_ref_cell));
     let _serial = sht40.serial_number(&mut delay);
 
-    lcd.backlight(true)
-        .map_err(|_| mischief::Report::from_debug("Failed to initialize LCD"))?;
     lcd.print("Hello !")
         .map_err(|_| mischief::Report::from_debug("Failed to print initial message"))?;
     delay.delay_millis(2000);
+    lcd.clear()
+        .map_err(|_| mischief::Report::from_debug("Failed to clear screen"))?
+        .home()
+        .map_err(|_| {
+            mischief::Report::from_debug("Failed to set the cursor to the home position")
+        })?;
 
     // variables in loop
     let mut temperature: f32;
     let mut humidity: f32;
     let mut buf_temp: String<16> = String::new();
     let mut buf_humid: String<16> = String::new();
+
     loop {
-        lcd.clear()
-            .map_err(|_| mischief::Report::from_debug("Failed to clear screen"))?
-            .home()
-            .map_err(|_| {
-                mischief::Report::from_debug("Failed to set the cursor to the home position")
-            })?;
+        delay.delay_millis(500);
+        lcd.backlight(true)
+            .map_err(|_| mischief::Report::from_debug("Failed to initialize LCD"))?;
 
         if let Ok(measurement) = sht40.measure(Precision::Low, &mut delay) {
             temperature = measurement.temperature_celsius().to_num();
@@ -79,12 +108,31 @@ fn app() -> mischief::Result<()> {
             write!(buf_humid, "Hum:  {:.2}%", humidity).map_err(mischief::Report::from_debug)?;
             lcd.write_str(&buf_temp)
                 .map_err(mischief::Report::from_debug)?;
-
             lcd.set_cursor(0, 1)
                 .map_err(|_| mischief::Report::from_debug("Failed to set cursor to line 2"))?;
             lcd.write_str(&buf_humid)
                 .map_err(mischief::Report::from_debug)?;
         }
+
+        // wait 5s and sleep
         delay.delay_millis(5000);
+        lcd.clear()
+            .map_err(|_| mischief::Report::from_debug("Failed to clear screen"))?
+            .home()
+            .map_err(|_| {
+                mischief::Report::from_debug("Failed to set the cursor to the home position")
+            })?
+            .print("Fall Sleep......")
+            .map_err(|_| mischief::Report::from_debug("Failed to show text"))?;
+        delay.delay_millis(500);
+        lcd.backlight(false)
+            .map_err(|_| mischief::Report::from_debug("Failed to disable backlight"))?
+            .clear()
+            .map_err(|_| mischief::Report::from_debug("Failed to clear screen"))?
+            .home()
+            .map_err(|_| {
+                mischief::Report::from_debug("Failed to set the cursor to the home position")
+            })?;
+        rtc.sleep_light(&[&wakeup_source]);
     }
 }
