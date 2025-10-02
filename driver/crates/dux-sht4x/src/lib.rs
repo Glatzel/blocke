b365a0556cdd361e696a56a46bb1048b2ef358d9
Glatzel <893016099@qq.com>
2025-10-02 22:34:06 +0800

    f

--- driver/crates/dux-sht4x/src/lib.rs
+++ driver/crates/dux-sht4x/src/lib.rs
@@ -4,12 +4,5 @@ mod primitives;
 mod responses;
 
 pub use primitives::*;
-#[cfg(not(feature = "async"))]
 mod sht4x;
-#[cfg(not(feature = "async"))]
 pub use sht4x::*;
-#[cfg(feature = "async")]
-mod sht4x_async;
-
-#[cfg(feature = "async")]
-pub use sht4x_async::*;
--- driver/crates/dux-sht4x/src/sht4x.rs
+++ driver/crates/dux-sht4x/src/sht4x.rs
@@ -1,8 +1,17 @@
 use core::marker::PhantomData;
 
+#[cfg(not(feature = "async"))]
 use embedded_hal::delay::DelayNs;
+#[cfg(not(feature = "async"))]
 use embedded_hal::i2c::I2c;
+#[cfg(feature = "async")]
+use embedded_hal_async::delay::DelayNs;
+#[cfg(feature = "async")]
+use embedded_hal_async::i2c::I2c;
+#[cfg(not(feature = "async"))]
 use sensirion_i2c::i2c;
+#[cfg(feature = "async")]
+use sensirion_i2c::i2c_async;
 
 use crate::error::Sht4xError;
 use crate::primitives::{Address, Command, HeatingDuration, HeatingPower, Measurement, Precision};
@@ -40,7 +49,13 @@ where
 
     /// Destroys the driver and returns the used I2C bus.
     pub fn destroy(self) -> I { self.i2c }
-
+}
+#[cfg(not(feature = "async"))]
+impl<I, D> Sht4x<I, D>
+where
+    I: I2c,
+    D: DelayNs,
+{
     /// Performs a measurement returning measurands in SI units.
     pub fn measure(
         &mut self,
@@ -105,6 +120,81 @@ where
         let mut response = [0; RESPONSE_LEN];
 
         i2c::read_words_with_crc(&mut self.i2c, self.address.into(), &mut response)?;
+
+        Ok(response)
+    }
+}
+#[cfg(feature = "async")]
+impl<I, D> Sht4x<I, D>
+where
+    I: I2c,
+    D: DelayNs,
+{
+    /// Performs a measurement returning measurands in SI units.
+    pub async fn measure(
+        &mut self,
+        precision: Precision,
+        delay: &mut D,
+    ) -> Result<Measurement, Sht4xError<I::Error>> {
+        let command = Command::from(precision);
+
+        self.send_command(command, delay).await?;
+        let response = self.read_response().await?;
+        let raw = sensor_data_from_response(response);
+
+        Ok(Measurement::from(raw))
+    }
+    /// Activates the heater and performs a measurement returning measurands in
+    /// SI units.
+    ///
+    /// **Note:** The heater is designed to be used up to 10 % of the sensor's
+    /// lifetime.
+    pub async fn heat_and_measure(
+        &mut self,
+        power: HeatingPower,
+        duration: HeatingDuration,
+        delay: &mut D,
+    ) -> Result<Measurement, Sht4xError<I::Error>> {
+        let command = Command::from((power, duration));
+
+        self.send_command(command, delay).await?;
+        let response = self.read_response().await?;
+        let raw = sensor_data_from_response(response);
+
+        Ok(Measurement::from(raw))
+    }
+
+    /// Reads the sensor's serial number.
+    pub async fn serial_number(&mut self, delay: &mut D) -> Result<u32, Sht4xError<I::Error>> {
+        self.send_command(Command::SerialNumber, delay).await?;
+        let response = self.read_response().await?;
+        Ok(serial_number_from_response(response))
+    }
+
+    /// Performs a soft reset of the sensor.
+    pub async fn soft_reset(&mut self, delay: &mut D) -> Result<(), Sht4xError<I::Error>> {
+        self.send_command(Command::SoftReset, delay).await
+    }
+
+    async fn send_command(
+        &mut self,
+        command: Command,
+        delay: &mut D,
+    ) -> Result<(), Sht4xError<I::Error>> {
+        let code = command.code();
+
+        i2c_async::write_command_u8(&mut self.i2c, self.address.into(), code)
+            .await
+            .map_err(Sht4xError::I2cWrite)?;
+        delay.delay_ms(command.duration_ms()).await;
+
+        Ok(())
+    }
+
+    async fn read_response(&mut self) -> Result<[u8; RESPONSE_LEN], Sht4xError<I::Error>> {
+        let mut response = [0; RESPONSE_LEN];
+
+        i2c_async::read_words_with_crc(&mut self.i2c, self.address.into(), &mut response).await?;
 
         Ok(response)
     }
