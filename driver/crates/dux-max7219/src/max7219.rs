17df73e9b230f69198bc38278fef2c6910ffb348
Glatzel <893016099@qq.com>
2025-10-04 15:07:52 +0800

    async

--- driver/crates/dux-max7219/src/max7219.rs
+++ driver/crates/dux-max7219/src/max7219.rs
@@ -1,4 +1,7 @@
+#[cfg(not(feature = "async"))]
 use embedded_hal::spi::{Error, SpiDevice};
+#[cfg(feature = "async")]
+use embedded_hal_async::spi::{Error, SpiDevice};
 use paste::paste;
 
 use crate::SegmentChar;
@@ -23,6 +26,7 @@ macro_rules! max7219_new {
         }
     };
 }
+#[cfg(not(feature = "async"))]
 macro_rules! impl_setter {
     ($name:ident, $cmd:ident, $ty:ty) => {
         paste! {
@@ -39,19 +43,30 @@ macro_rules! impl_setter {
         }
     };
 }
+#[cfg(feature = "async")]
+macro_rules! impl_setter {
+    ($name:ident, $cmd:ident, $ty:ty) => {
+        paste! {
+            pub fn $name(&mut self, index: usize, value: $ty) -> Result<&mut Self, Max7219Error> {
+               self.command(index, Command::$cmd, value as u8)
+            }
+            pub async fn [<$name _checked>](
+                &mut self,
+                index: usize,
+                value: $ty,
+            ) -> Result<&mut Self, Max7219Error> {
+                self.command_checked(index, Command::$cmd, value as u8).await
+            }
+        }
+    };
+}
 
 impl<SPI, const N: usize, const BUF: usize> Max7219<SPI, N, BUF>
 where
     SPI: SpiDevice,
 {
     pub const fn count(&self) -> usize { N }
-    pub fn write(&mut self) -> Result<&mut Self, Max7219Error> {
-        self.device
-            .write(&self.buffer)
-            .map_err(|e| Max7219Error::SpiWriteError(e.kind()))?;
-        self.updated.fill(false);
-        Ok(self)
-    }
+
     pub fn command(
         &mut self,
         index: usize,
@@ -62,6 +77,43 @@ where
         self.buffer[2 * index + 1] = command as u8;
 
         self.updated[index] = true;
+        Ok(self)
+    }
+
+    pub fn set_digit(
+        &mut self,
+        device_index: usize,
+        digit_index: u8,
+        data: u8,
+    ) -> Result<&mut Self, Max7219Error> {
+        self.command(device_index, digit_index.try_into()?, data)
+    }
+
+    pub fn set_segment_char(
+        &mut self,
+        device_index: usize,
+        segment_index: u8,
+        character: SegmentChar,
+        with_dot: bool,
+    ) -> Result<&mut Self, Max7219Error> {
+        self.set_digit(
+            device_index,
+            segment_index,
+            character as u8 | ((with_dot as u8) << 7),
+        )
+    }
+}
+
+#[cfg(not(feature = "async"))]
+impl<SPI, const N: usize, const BUF: usize> Max7219<SPI, N, BUF>
+where
+    SPI: SpiDevice,
+{
+    pub fn write(&mut self) -> Result<&mut Self, Max7219Error> {
+        self.device
+            .write(&self.buffer)
+            .map_err(|e| Max7219Error::SpiWriteError(e.kind()))?;
+        self.updated.fill(false);
         Ok(self)
     }
     pub fn command_checked(
@@ -83,14 +135,6 @@ where
     impl_setter!(set_intensity, Intensity, Intensity);
     impl_setter!(set_scan_limit, ScanLimit, ScanLimit);
     impl_setter!(set_display_test, DisplayTest, bool);
-    pub fn set_digit(
-        &mut self,
-        device_index: usize,
-        digit_index: u8,
-        data: u8,
-    ) -> Result<&mut Self, Max7219Error> {
-        self.command(device_index, digit_index.try_into()?, data)
-    }
     pub fn set_digit_checked(
         &mut self,
         device_index: usize,
@@ -98,19 +142,6 @@ where
         data: u8,
     ) -> Result<&mut Self, Max7219Error> {
         self.command_checked(device_index, digit_index.try_into()?, data)
-    }
-    pub fn set_segment_char(
-        &mut self,
-        device_index: usize,
-        segment_index: u8,
-        character: SegmentChar,
-        with_dot: bool,
-    ) -> Result<&mut Self, Max7219Error> {
-        self.set_digit(
-            device_index,
-            segment_index,
-            character as u8 | ((with_dot as u8) << 7),
-        )
     }
     pub fn set_segment_char_checked(
         &mut self,
@@ -126,9 +157,59 @@ where
         )
     }
 }
-impl<SPI, const N: usize, const BUF: usize> Drop for Max7219<SPI, N, BUF>
+#[cfg(feature = "async")]
+impl<SPI, const N: usize, const BUF: usize> Max7219<SPI, N, BUF>
 where
     SPI: SpiDevice,
 {
-    fn drop(&mut self) { self.write().unwrap(); }
+    pub async fn write(&mut self) -> Result<&mut Self, Max7219Error> {
+        self.device
+            .write(&self.buffer)
+            .await
+            .map_err(|e| Max7219Error::SpiWriteError(e.kind()))?;
+        self.updated.fill(false);
+        Ok(self)
+    }
+    pub async fn command_checked(
+        &mut self,
+        index: usize,
+        command: Command,
+        data: u8,
+    ) -> Result<&mut Self, Max7219Error> {
+        if index >= N {
+            return Err(Max7219Error::IndexOutOfBounds { index, bound: N });
+        }
+        if self.updated[index] == true {
+            self.write().await?;
+        }
+        self.command(index, command, data)
+    }
+    impl_setter!(set_power, Power, bool);
+    impl_setter!(set_decode_mode, DecodeMode, DecodeMode);
+    impl_setter!(set_intensity, Intensity, Intensity);
+    impl_setter!(set_scan_limit, ScanLimit, ScanLimit);
+    impl_setter!(set_display_test, DisplayTest, bool);
+    pub async fn set_digit_checked(
+        &mut self,
+        device_index: usize,
+        digit_index: u8,
+        data: u8,
+    ) -> Result<&mut Self, Max7219Error> {
+        self.command_checked(device_index, digit_index.try_into()?, data)
+            .await
+    }
+    pub async fn set_segment_char_checked(
+        &mut self,
+        device_index: usize,
+        segment_index: u8,
+        character: SegmentChar,
+        with_dot: bool,
+    ) -> Result<&mut Self, Max7219Error> {
+        self.set_digit_checked(
+            device_index,
+            segment_index,
+            character as u8 | ((with_dot as u8) << 7),
+        )
+        .await
+    }
 }
