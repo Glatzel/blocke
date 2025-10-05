9877dc1729aac880928c6c63ac30b01128ac5020
Glatzel <893016099@qq.com>
2025-10-05 15:05:26 +0800

    f

--- esp32-c3/crates/esp32-c3-zero-example/src/bin/hello_world.rs
+++ esp32-c3/crates/esp32-c3-zero-example/src/bin/hello_world.rs
@@ -1,16 +1,19 @@
 #![no_std]
 #![no_main]
 
+use defmt::println;
 use esp_hal::delay::Delay;
 use esp_hal::main;
-use esp_println::println;
 use pain as _;
 esp_bootloader_esp_idf::esp_app_desc!();
+
+use esp_println as _;
 #[main]
 fn main() -> ! {
     let delay = Delay::new();
     loop {
         println!("hello world");
+        clerk::info!("hello world");
         delay.delay_millis(1000);
     }
 }
